950f320ca4bf8ae414e1fdadeff3dc29c79ef0d9
serhii <aserhii@protonmail.com>
2019-07-27 21:22:03 +0300

    Create maps and add Country model

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,22 @@
+"""Models for Maps app."""
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    """Create Country model for Maps app."""
+
+    name = models.CharField(max_length=200)
+    freedomhouse_score = models.FloatField(blank=True, null=True)
+    quality_of_life_index = models.FloatField(blank=True, null=True)
+    purchasing_power_index = models.FloatField(blank=True, null=True)
+    safety_index = models.FloatField(blank=True, null=True)
+    health_care_index = models.FloatField(blank=True, null=True)
+    cost_of_living_index = models.FloatField(blank=True, null=True)
+    property_price_to_income_ratio = models.FloatField(blank=True, null=True)
+    traffic_commute_time_index = models.FloatField(blank=True, null=True)
+    pollution_index = models.FloatField(blank=True, null=True)
+    climate_index = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        """Return country name in admin."""
+        return self.name
--- maps/tests.py
+++ maps/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Country
+
+
+class CountryModelTests(TestCase):
+    """Tests for post model."""
+
+    def setUp(self):
+        """Set up non-modifiable objects used by all test methods."""
+        self.country = Country(
+            name="Narnia",
+            freedomhouse_score="15",
+            quality_of_life_index="20",
+            purchasing_power_index="",
+            safety_index="2",
+            health_care_index="10",
+            cost_of_living_index="70",
+            property_price_to_income_ratio="30",
+            traffic_commute_time_index="20",
+            pollution_index="5",
+            climate_index="10",
+        )
+
+    def tearDown(self):
+        """Clean-up test data."""
+        del self.country
+
+    def test_string_representation(self):
+        """Test string representation country."""
+        self.assertEqual(str(self.country), self.country.name)
