da84a77d0eb508f1fd30b9a59ed3587923ba1b72
serhii <aserhii@protonmail.com>
2019-07-28 22:54:53 +0300

    Add react

--- place2live_com/urls.py
+++ place2live_com/urls.py
@@ -18,4 +18,8 @@ from django.urls import include
 from django.urls import path
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("maps.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("maps.urls")),
+    path("", include("frontend.urls")),
+]
