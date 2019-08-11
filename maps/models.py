17e8af2ba5b28f0f1e3aa948ef83b18f0e5e2b5c
serhii <aserhii@protonmail.com>
2019-08-11 22:24:42 +0300

    Add app maps

---
+++ maps/models.py
@@ -0,0 +1,13 @@
+"""Models for Maps app."""
+from django.db import models
+
+
+class Country(models.Model):
+    """Create Country model for Maps app."""
+
+    name = models.CharField(max_length=200)
+    population = models.DecimalField(blank=True, null=True)
+
+    def __str__(self):
+        """Return country name in admin."""
+        return self.name
---
+++ maps/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def show_map(request):
+    return HttpResponse('Hello, world!')
